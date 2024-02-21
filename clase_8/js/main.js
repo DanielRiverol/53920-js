7a490d9b41438e82c26adde94704d414101fe270
Daniel Riverol <dannyriverol@hotmail.com>
2024-02-20 21:16:11 -0300

    Clase 8 Funciones de oriden Superior II

---
+++ clase_8/js/main.js
@@ -0,0 +1,10 @@
+const tickets3D = [
+  { id: 1, pelicula: "ant-man quantunmania", precio: 2400 },
+  { id: 2, pelicula: "super mario bros, la película", precio: 2300 },
+  { id: 3, pelicula: "john wick 4", precio: 2600 },
+  { id: 4, pelicula: "the wale", precio: 2200 },
+  { id: 5, pelicula: "shazam, la furia de los dioses", precio: 2575 },
+  { id: 6, pelicula: "creed 3", precio: 2389 },
+  { id: 7, pelicula: "the marvels", precio: 1500 },
+];
+
