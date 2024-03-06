4e639d75f7cbdae720a90299ef640e821df7d865
Daniel Riverol <dannyriverol@hotmail.com>
2024-03-06 12:04:46 -0300

    Clase 12 Operadores Avanzados

--- clase_12/js/main.js
+++ clase_12/js/main.js
@@ -44,31 +44,127 @@ function crearHtml(arr) {
   //validar qué pasa cuando no recibo ningun array
   let html;
   for (const el of arr) {
+
+const {img, nombre, precio, id}= el
+
     html = `<div class="card">
-                <img src=" ./img/${el.img}" alt="${el.nombre}">
+                <img src=" ./img/${img}" alt="${nombre}">
                 <hr>
-                <h3>${el.nombre}</h3>
-                <p>Precio: $${el.precio} </p>
+                <h3>${nombre}</h3>
+                <p>Precio: $${precio} </p>
                   <div class="card-action">
-                    <button class="btn btn-success" id="${el.id}">Agregar</button>
+                    <button class="btn btn-success" id="${id}">Agregar</button>
                   </div>
               </div>`;
     //se la agrego al contenedor
-    contenedor.innerHTML = contenedor.innerHTML + html;
+    contenedor.innerHTML += html;
   }
 }
+crearHtml(servicios)
+//Crear una funcion que guarde la preferencia del usuario si desea recordar o no susudatos
+const inputUser = document.querySelector("#user"),
+  inputPass = document.querySelector("#pass"),
+  checkbox = document.querySelector("#check"),
+  form = document.querySelector("form");
+//console.log(form);
+function guardarEnLS(storage) {
+  //funcionalidad
+  const user = {
+    usuario: inputUser.value,
+    pass: inputPass.value,
+  };
+  // if (storage == "local") {
+  //   localStorage.setItem("usuario", JSON.stringify(user));
+  // }
+  // if (storage == "session") {
+  //   sessionStorage.setItem("usuario", JSON.stringify(user));
+  // }
+  storage == "local" && localStorage.setItem("usuario", JSON.stringify(user));
+  storage == "session" &&
+    sessionStorage.setItem("usuario", JSON.stringify(user));
+}
 
- //Crear una funcion que guarde la preferencia del usuario si desea recordar o no susudatos
- const inputUser = document.querySelector("#user"),
-   inputPass = document.querySelector("#pass"),
-   checkbox = document.querySelector("#check"),
-   form = document.querySelector('form');
+form?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // if (checkbox.checked) {
+  //   guardarEnLS("local");
+  // } else {
+  //   guardarEnLS("session");
+  // }
+  checkbox.checked ? guardarEnLS("local") : guardarEnLS("session");
+});
 
-   function guardarEnLS(storage){
-    //funcionalidad
-   }
+let temp = 29;
+// if (temp > 29) {
+//   console.log("Dia caluroso");
+// } else {
+//   console.log("Dia agradable");
+// }
+// //condicion ? true : false
+// temp > 29 ? console.log("Dia caluroso") : console.log("Dia agradable");
+const persona = {
+  nombre: "Juanito",
+  apellido: "Arcoiris",
+  edad: 18,
+  activo: false,
+};
+// let permiso;
+// if (persona.edad >= 18) {
+//   permiso = true;
+// } else {
+//   permiso = false;
+// }
 
-   form.addEventListener('submit',(e)=>{
-        e.preventDefault()
-        guardarEnLS('')
-   })
+// if(permiso){
+//   console.log("Podes entrar al casino");
+// }else{
+//   console.log("NO Podes entrar al casino");
+// }
+let permiso = persona.edad >= 18 ? true : false;
+//console.log(permiso);
+// permiso
+//   ? console.log("Podes entrar al casino")
+//   : console.log("NO Podes entrar al casino");
+//op AND
+let carrito = [0];
+// if(carrito.length === 0){
+//   contenedor.innerHTML= "El carrito esta vacio"
+// }
+carrito.length === 0 && (contenedor.innerHTML = "El carrito esta vacio");
+
+let estado = persona.activo && "Cuota al dia";
+
+//console.log(estado);
+const persona1 = {
+  nombre: "Juanito",
+  apellido: "Arcoiris",
+  edad: 18,
+  activo: false,
+};
+let n1 = null;
+const persona2 = {
+  id_persona: 1,
+  nombre: "Lisa",
+  apellido: "Simpson",
+  edad: 8,
+};
+
+// localStorage.setItem("carrito", JSON.stringify({ nombre: "Juan" }));
+// const carrito2= JSON.parse(localStorage.getItem('carrito')) || [];
+// console.log(localStorage.getItem("carrito"));
+// console.log(carrito2);
+
+// console.log(persona2?.nombre || "la persona no existe");
+// console.log(persona1.nombre || "la persona no existe");
+//Destructuring
+// let nombre= persona1.nombre
+// let edad= persona1.edad
+
+const { nombre, apellido, activo, edad } = persona1;
+const { nombre: nombre_pers , id_persona:id} = persona2; //alias
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+console.log(nombre_pers);
+console.log(id);
