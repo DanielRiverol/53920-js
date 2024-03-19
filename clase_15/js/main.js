8d164c2cfcb2d68a16bd35f4252786d8164f3183
Daniel Riverol <dannyriverol@hotmail.com>
2024-03-19 10:33:48 -0300

    Clase 15 Asincronía y promesas

--- clase_15/js/main.js
+++ clase_15/js/main.js
@@ -1,3 +1,6 @@
+// setTimeout(() => {
+//   console.log("Mitad del proceso");
+// }, 3000);
 const card = document.querySelector(".card");
 const btn = document.querySelector("#btn"),
   contenedor = document.querySelector(".contenedor");
@@ -33,4 +36,93 @@ function logSquare(num) {
   console.log(square(num));
 }
 
-//logSquare(2);
+//logSquare(4);
+//1s = 1000ms
+// setTimeout(() => {
+//   console.log("Soy una instruccion asincrona");
+// }, 3000);
+// console.log("Inicio del proceso");
+
+// console.log("Final del proceso");
+
+btn.addEventListener("click", () => {
+  setTimeout(() => {
+    card.classList.remove("active");
+  }, 2500);
+  card.classList.add("active");
+});
+
+// setTimeout(() => {
+//   for (const letra of "Hola") {
+//     console.log(letra);
+//   }
+// }, 3000);
+// setTimeout(() => {
+//   for (const letra of "Santiago") {
+//     console.log(letra);
+//   }
+// }, 2000);
+
+// setTimeout(() => {
+//   console.log("Mitad del proceso");
+// }, 0);
+// console.log("Inicio del proceso");
+// console.log("Final del proceso");
+//setInterval
+// let contador = 0;
+// let detener = false;
+// const intervalo = setInterval(() => {
+//   contador++;
+//   if (contador == 10) {
+//     clearInterval(intervalo);
+//   }
+//   console.log("El valor de contador es :" + contador);
+// }, 1000);
+
+//Promesas
+new Promise((resolve, reject) => {
+  //cuerpo de la promesa
+});
+
+const eventoFuturo = (valor) => {
+  return new Promise((resolve, reject) => {
+    if (valor == true) {
+      resolve("Promesa resuelta");
+    } else {
+      reject("Promesa rechazada");
+    }
+  });
+};
+// console.log(eventoFuturo(true)); //Resuelta
+// console.log(eventoFuturo(false)); //Rechazada
+// console.log(eventoFuturo()); //Rechazada
+
+// //Métodos then y catch
+// eventoFuturo(true).then((res) => {
+//   console.log(res); //capturo el valor del resolve
+// });
+// eventoFuturo(false).catch((error) => {
+//   console.log(error); //capturo el valor del reject
+// });
+
+//Pedir CD
+const devolverCd = (respuesta) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (respuesta === "si") {
+        resolve("Si! Gracias por prestalo");
+      } else {
+        reject("No, lo perdí :(");
+      }
+    }, 3000);
+  });
+};
+//Prueba
+console.log("Hola! ¿Te acordás que te presté un cd hace un tiempo?");
+devolverCd()
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
